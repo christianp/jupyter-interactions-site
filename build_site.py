b7049bb548283eccd567658c59c5f608af70691b
Christian Perfect <christianperfect@gmail.com>
2017-01-17 16:35:21 +0000

    first commit
    We can create a single page listing all of the notebooks

---
+++ build_site.py
@@ -0,0 +1,59 @@
+import os
+import jinja2
+import shutil
+from get_metadata import Notebook
+from distutils.dir_util import copy_tree
+import yaml
+
+class Site(object):
+    def __init__(self,build_path='build',template_path='templates',static_path='static',site_context={}):
+        self.build_path = build_path
+        try:
+            os.mkdir(self.build_path)
+        except FileExistsError:
+            pass
+
+        self.static_path = static_path
+
+        self.site_context = site_context
+
+        self.template_path = template_path
+        self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.template_path))
+
+    def make_file(self,template_name,destination,context):
+        template = self.env.get_template(template_name)
+        ctx = {}
+        ctx.update({'site':self.site_context})
+        ctx.update(context)
+        open(os.path.join(self.build_path,destination),'w').write(template.render(ctx))
+
+    def copy_static(self):
+        copy_tree(self.static_path, os.path.join(self.build_path,'static'))
+
+    def build(self):
+        self.copy_static()
+
+class NotebookSite(Site):
+    def __init__(self,notebook_path='.',*args,**kwargs):
+        self.notebook_path = notebook_path
+        super(NotebookSite,self).__init__(*args,**kwargs)
+        self.load_notebooks()
+
+    def load_notebooks(self):
+        self.notebooks = []
+        for filename in os.listdir(self.notebook_path):
+            name,ext = os.path.splitext(filename)
+            if ext=='.ipynb':
+                notebook = Notebook(os.path.join(self.notebook_path,filename),filename)
+                self.notebooks.append(notebook)
+
+    def build(self):
+        super(NotebookSite,self).build()
+        site.make_file('gallery.html','gallery.html',{'notebooks':self.notebooks})
+
+        for notebook in self.notebooks:
+            pass
+
+config = yaml.load(open('config.yml').read())
+site = NotebookSite(**config)
+site.build()
---
+++ get_metadata.py
@@ -0,0 +1,77 @@
+import json
+import re
+import jmespath
+import codecs
+from markdown_mistune import markdown2html_mistune
+
+def handle_svg(data):
+    source = ''.join(data)
+    return source
+
+def handle_png(data):
+    return '<img src="data:image/png;base64,{}">'.format(data)
+
+image_handlers = {
+    'image/svg+xml': handle_svg,
+    'image/png': handle_png,
+}
+
+class NotebookInvalidException(Exception):
+    def __init__(self,message,notebook):
+        self.notebook = notebook
+        self.message = message
+
+    def __str__(self):
+        return 'Notebook "{}" is invalid: {}'.format(self.notebook.filename, self.message)
+
+class Notebook(object):
+    def __init__(self,file_path,filename):
+        self.filename = filename
+        self.data = json.loads(codecs.open(file_path,encoding='utf-8').read())
+        self.get_metadata()
+        self.get_image()
+        try:
+            self.validate()
+            self.valid = True
+        except NotebookInvalidException:
+            self.valid = False
+
+    def get_metadata(self):
+        re_title = re.compile(r'^#\s+(.*)$',re.MULTILINE)
+        re_author = re.compile(r'^##\s+Author:\s+(.*)$',re.MULTILINE)
+
+        text = ''.join(jmespath.search('cells[0].source',self.data))
+        if not text:
+            return
+
+        m = re_title.search(text)
+        self.title = 'arg'
+        if m:
+            self.title = m.group(1)
+        m = re_author.search(text)
+        if m:
+            self.author = m.group(1)
+            self.description = markdown2html_mistune(text[m.end()+1:].strip())
+
+    def get_image(self):
+        for output in jmespath.search('cells[].outputs[].data',self.data):
+            mime_types = [k for k in output if k in image_handlers]
+            if len(mime_types):
+                mime_type = mime_types[0]
+                image_data = output[mime_type]
+                image_tag = image_handlers[mime_type](image_data)
+                self.image = {
+                    'mime_type': mime_type,
+                    'image_data': image_data,
+                    'html': image_tag,
+                }
+                return
+
+    def validate(self):
+        if not hasattr(self,'image'):
+            raise NotebookInvalidException("No image",self)
+
+
+if __name__ == '__main__':
+    notebook = Notebook('Divisors of a positive integers.ipynb')
+    print(notebook)
